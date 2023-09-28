4cb44047044e4e10e4ebb1f2324f66c6368bb713
Lenni0451 <20379977+Lenni0451@users.noreply.github.com>
2023-09-28 18:12:29 +0200

    Updated plugin and improved code

--- src/main/java/net/lenni0451/nolocalconnections/Main.java
+++ src/main/java/net/lenni0451/nolocalconnections/Main.java
@@ -20,30 +20,19 @@ public class Main extends ViaProxyPlugin {
         if (this.isLocal(betterAddress)) event.setCancelMessage("§CYou can't connect to any local address");
     }
 
-    private boolean isLocal(final InetAddress addr) {
-        String hostAddress = addr.getHostAddress();
-        return addr.isAnyLocalAddress()
-                || addr.isLoopbackAddress()
-                || hostAddress.endsWith(".0")
-                || hostAddress.endsWith(".255")
-                || hostAddress.startsWith("192.168.")
-                || hostAddress.startsWith("172.16.")
-                || hostAddress.startsWith("172.17.")
-                || hostAddress.startsWith("172.18.")
-                || hostAddress.startsWith("172.19.")
-                || hostAddress.startsWith("172.20.")
-                || hostAddress.startsWith("172.21.")
-                || hostAddress.startsWith("172.22.")
-                || hostAddress.startsWith("172.23.")
-                || hostAddress.startsWith("172.24.")
-                || hostAddress.startsWith("172.25.")
-                || hostAddress.startsWith("172.26.")
-                || hostAddress.startsWith("172.27.")
-                || hostAddress.startsWith("172.28.")
-                || hostAddress.startsWith("172.29.")
-                || hostAddress.startsWith("172.30.")
-                || hostAddress.startsWith("172.31.")
-                || hostAddress.startsWith("10.");
+    private boolean isLocal(final InetAddress address) {
+        if (address.isAnyLocalAddress() || address.isLoopbackAddress()) return true;
+        byte[] addressBytes = address.getAddress();
+
+        if (addressBytes.length == 4) { // Check for IPv4 local address ranges
+            if (addressBytes[0] == 10) return true; // 10.0.0.0/8
+            if (addressBytes[0] == (byte) 172 && addressBytes[1] >= 16 && addressBytes[1] <= 31) return true; // 172.16.0.0/12
+            if (addressBytes[0] == (byte) 192 && addressBytes[1] == (byte) 168) return true; // 192.168.0.0/16
+        }
+        if (addressBytes.length == 16) { // Check for IPv6 local address ranges
+            return (addressBytes[0] == (byte) 0xfe && (addressBytes[1] & (byte) 0xc0) == (byte) 0x80); // fe80::/10
+        }
+        return false;
     }
 
 }
